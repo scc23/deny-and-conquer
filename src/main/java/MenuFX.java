6b53618573cbb6895316326a358997aad6652dbb
scc23 <sherman_chow@sfu.ca>
2019-02-19 20:02:55 -0800

    Add server menu configurations

--- src/main/java/MenuFX.java
+++ src/main/java/MenuFX.java
@@ -1,18 +1,16 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
 
 public class MenuFX extends Application {
 
@@ -34,22 +32,21 @@ public class MenuFX extends Application {
         primaryStage.show();
     }
 
-    public Scene startMenuScene() {
+    // Start menu
+    private Scene startMenuScene() {
         Label label = new Label("Welcome to Deny & Conquer!");
-
         VBox root = new VBox(15);
 
-        Button hostGameBtn = new Button("Host a Game");
-        Button joinGameBtn = new Button("Join a Game");
-
-        // Display the server menu when hostGameBtn is clicked
+        // Host game button to display server menu
+        Button hostGameBtn = new Button("Host Game");
         hostGameBtn.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 stage.setScene(serverMenuScene());
             }
         });
 
-        // Display the client menu when joinGameBtn is clicked
+        // Join game button to display client menu
+        Button joinGameBtn = new Button("Join Game");
         joinGameBtn.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 stage.setScene(clientMenuScene());
@@ -62,7 +59,8 @@ public class MenuFX extends Application {
         return new Scene(root, 300, 300);
     }
 
-    protected Scene setupScene(HBox hbTitle, VBox vbox) {
+    // Function to include the necessary contents inside a scene
+    private Scene setupScene(HBox hbTitle, VBox vbox) {
         // Create back button to return to start menu
         HBox hbButtons = new HBox();
         Button backBtn = new Button("← Menu");
@@ -83,7 +81,7 @@ public class MenuFX extends Application {
         vbox.setAlignment(Pos.CENTER);
 
         BorderPane root = new BorderPane();
-        root.setPadding(new Insets(35, 0, 0, 0));
+        root.setPadding(new Insets(50, 0, 0, 0));
         root.setTop(hbTitle);
         root.setCenter(vbox);
         root.setBottom(hbButtons);
@@ -91,30 +89,55 @@ public class MenuFX extends Application {
         return new Scene(root, 300, 300);
     }
 
-    protected Scene serverMenuScene() {
+    // Function to create a dropdown menu
+    private HBox createComboBox(Label label, ObservableList<String> options) {
+        HBox hbox = new HBox();
+
+        final ComboBox comboBox = new ComboBox<String>(options);
+        comboBox.getSelectionModel().selectFirst();
+
+        hbox.getChildren().addAll(label, comboBox);
+        hbox.setAlignment(Pos.CENTER);
+
+        return hbox;
+    }
+
+    // Server menu
+    private Scene serverMenuScene() {
         HBox hbTitle = new HBox();
-        Label labelTitle = new Label("Host a Game");
+        Label labelTitle = new Label("Host Game");
         hbTitle.getChildren().add(labelTitle);
 
         VBox vbox = new VBox(15);
-        Label labelPenThickness = new Label("Thickness of pen:");
-        Label labelGameBoardSize = new Label("Size of game board:");
 
+        // Create dropdown menu for pen thickness configuration
+        Label labelPenThickness = new Label("Pen thickness: ");
+        ObservableList<String> penThicknessOptions = FXCollections.observableArrayList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+        HBox penConfig = createComboBox(labelPenThickness, penThicknessOptions);
+
+        // Create dropdown menu for pen game board size configuration
+        Label labelGameBoardSize = new Label("Game board size: ");
+        ObservableList<String> gameBoardSizeOptions = FXCollections.observableArrayList("2x2", "3x3", "4x4", "5x5", "6x6", "7x7", "8x8", "9x9", "10x10");
+        HBox gameBoardConfig = createComboBox(labelGameBoardSize, gameBoardSizeOptions);
+
+        // Start game/wait for more players
         Button startGameBtn = new Button("Start");
+        startGameBtn.setMaxSize(100,200);
         startGameBtn.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 System.out.println("Starting game...");
             }
         });
 
-        vbox.getChildren().addAll(labelPenThickness, labelGameBoardSize, startGameBtn);
+        vbox.getChildren().addAll(penConfig, gameBoardConfig, startGameBtn);
 
         return setupScene(hbTitle, vbox);
     }
 
-    protected Scene clientMenuScene() {
+    // Client menu
+    private Scene clientMenuScene() {
         HBox hbTitle = new HBox();
-        Label labelTitle = new Label("Join a Game");
+        Label labelTitle = new Label("Join Game");
         hbTitle.getChildren().add(labelTitle);
 
         VBox vbox = new VBox(15);
@@ -122,9 +145,9 @@ public class MenuFX extends Application {
         final TextField field = new TextField();
         field.setMaxWidth(150);
 
+        // Start game/wait for more players
         Button startGameBtn = new Button("Start");
-
-        // Display waiting scene when joinGameBtn is clicked
+        startGameBtn.setMaxSize(100,200);
         startGameBtn.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 System.out.println("Joining host ip address: " + field.getText());
