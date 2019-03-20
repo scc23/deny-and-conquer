a8a183bdfa824237a9d1c7b895700387e5c5e3ca
scc23 <sherman_chow@sfu.ca>
2019-03-20 15:55:29 -0700

    Add fields to GameMessage and implement methods for CoreGameClient

--- src/main/java/com/asap/dnc/core/CoreGameClient.java
+++ src/main/java/com/asap/dnc/core/CoreGameClient.java
@@ -1,20 +1,56 @@
 package com.asap.dnc.core;
 
 import com.asap.dnc.network.ClientInfo;
+import com.asap.dnc.network.MessageType;
 
 import java.io.*;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
+import java.net.*;
 
-public class CoreGameClient {
-    private ClientInfo hostServer;
+public class CoreGameClient implements CoreGameClientInterface {
+    private DatagramSocket socket;
+//    private ClientInfo hostServer;
 //    private Grid clientGrid;
 
-    // Send message to server for grid operation
-    void sendServerRequest(String address, int port, GameMessage msg) throws IOException {
+    private CoreGameClient() throws SocketException {
+        socket = new DatagramSocket();
     }
 
-    void receiveServerResponse() throws IOException, ClassNotFoundException {
-        // TODO: Receive response from server via multicast and perform operation on own grid
+    // Send message to server to validate grid operation
+    public void sendServerRequest(String address, int port, GameMessage msg) throws IOException {
+        InetAddress serverAddress = InetAddress.getByName(address);
+
+        ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bStream);
+
+        oos.writeObject(msg);
+        oos.flush();
+
+        byte[] buf = bStream.toByteArray();
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, serverAddress, port);
+        socket.send(packet);
+    }
+
+    // Receive response from server
+    public void receiveServerResponse() throws IOException, ClassNotFoundException {
+        // Timeout if no response from server
+        socket.setSoTimeout(2000);
+
+        while(true) {
+            try {
+                byte[] buf = new byte[5000];
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                ByteArrayInputStream inputByteStream = new ByteArrayInputStream(buf);
+                ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(inputByteStream));
+                GameMessage msg = (GameMessage)ois.readObject();
+            } catch (SocketTimeoutException e) {
+                System.out.println("Timeout reached: " + e);
+            }
+        }
+    }
+
+    // Execute grid operation
+    public void executeGridOperation(MessageType type) {
+
     }
 }
--- src/main/java/com/asap/dnc/core/GameMessage.java
+++ src/main/java/com/asap/dnc/core/GameMessage.java
@@ -1,12 +1,16 @@
 package com.asap.dnc.core;
 
+import com.asap.dnc.network.ClientInfo;
 import com.asap.dnc.network.Message;
 import com.asap.dnc.network.MessageType;
 
 import java.sql.Timestamp;
 
 public class GameMessage extends Message {
-    private int row, col;
+    private boolean isValid;    // Server will set this field in response message
+    private int row, col;       // Used for cell index
+    private PenColor color;     // Used to identify player
+    private boolean isOwned = false;    // Used to determine if cell is owned by player
     // TODO: finalFillPercentage is fixed for now, needs to be updated to reflect actual changes
     private double finalFillPercentage = 80;
 
@@ -14,26 +18,55 @@ public class GameMessage extends Message {
         super(type, timestamp);
     }
 
-    // Setters and getters for row and col
+    // Maybe we should use this instead?
+//    public GameMessage(MessageType type, Timestamp timestamp, ClientInfo clientInfo){
+//        super(type, timestamp);
+//        this.color = clientInfo.getPenColor();
+//    }
+
+    // Setter for row
     public void setRow(int row) {
         this.row = row;
     }
 
+    // Setter for col
     public void setCol(int col) {
         this.col = col;
     }
 
+    // Getter for row
     public int getRow() {
         return this.row;
     }
 
+    // Getter for col
     public int getCol() {
         return this.col;
     }
 
+    // Getter for pen color
+    public PenColor getPenColor() {
+        return this.color;
+    }
+
+    // Setter isOwned
+    public void setIsOwned() {
+        this.isOwned = true;
+    }
+
+    // Getter for isOwned
+    public boolean getIsOwned() {
+        return this.isOwned;
+    }
+
     // Getter for final fill percentage
     public double getFinalFillPercentage() {
-        return finalFillPercentage;
+        return this.finalFillPercentage;
+    }
+
+    // Getter for isValid
+    public boolean getIsValid() {
+        return this.isValid;
     }
 
     @Override
