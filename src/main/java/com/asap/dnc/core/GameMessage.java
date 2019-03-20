332e5a98712ce87acaa077a41da3a6e6d06b9342
scc23 <sherman_chow@sfu.ca>
2019-03-20 12:14:38 -0700

    Add getters and setters for game message grid row and col

--- src/main/java/com/asap/dnc/core/GameMessage.java
+++ src/main/java/com/asap/dnc/core/GameMessage.java
@@ -6,16 +6,32 @@ import com.asap.dnc.network.MessageType;
 import java.sql.Timestamp;
 
 public class GameMessage extends Message {
-    private Cell cell;
-    private double finalFillPercentage = 80.2;
+    private int row, col;
+    // TODO: finalFillPercentage is fixed for now, needs to be updated to reflect actual changes
+    private double finalFillPercentage = 80;
 
     public GameMessage(MessageType type, Timestamp timestamp){
         super(type, timestamp);
     }
-    public Cell getCell() {
-        return cell;
+
+    // Setters and getters for row and col
+    public void setRow(int row) {
+        this.row = row;
     }
 
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
+
+    // Getter for final fill percentage
     public double getFinalFillPercentage() {
         return finalFillPercentage;
     }
