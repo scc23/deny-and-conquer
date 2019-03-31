74acb3814d5b282776ae8b1d584fc3d0ef48d77e
zpzhou <zpzhou@sfu.ca>
2019-03-30 19:32:33 -0700

    updating EndGameHandler interface

--- src/main/java/com/asap/dnc/core/EndGameHandler.java
+++ src/main/java/com/asap/dnc/core/EndGameHandler.java
@@ -3,5 +3,6 @@ package com.asap.dnc.core;
 import java.util.Map;
 
 public interface EndGameHandler {
+    public void addThread(Thread t);
     public void onGameEnd(Map<PenColor, Integer> cellMap);
 }
